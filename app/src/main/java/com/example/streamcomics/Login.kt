2bd8347fde7979d714256701e59c29d09f1a1fd9
Carlos Sánchez Abad <98030188+thecharlie00@users.noreply.github.com>
2022-12-18 20:20:24 +0100

    Progreso 2
    Falta acabar de hacer recycler view

--- app/src/main/java/com/example/streamcomics/Login.kt
+++ app/src/main/java/com/example/streamcomics/Login.kt
@@ -1,11 +1,21 @@
 package com.example.streamcomics
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.streamcomics.databinding.ActivityLoginBinding
 
+private lateinit var binding: ActivityLoginBinding
 class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        binding.back.setOnClickListener{
+            startActivity(Intent(this, LoginRegister::class.java))
+        }
+        binding.next.setOnClickListener{
+            startActivity(Intent(this, Library::class.java))
+        }
     }
 }
--- app/src/main/java/com/example/streamcomics/Register.kt
+++ app/src/main/java/com/example/streamcomics/Register.kt
@@ -1,11 +1,21 @@
 package com.example.streamcomics
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.streamcomics.databinding.ActivityRegisterBinding
 
+private  lateinit var binding: ActivityRegisterBinding
 class Register : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        binding.backRegister.setOnClickListener{
+            startActivity(Intent(this, LoginRegister::class.java))
+        }
+        binding.nextRegister.setOnClickListener{
+            startActivity(Intent(this, Library::class.java))
+        }
     }
 }
