85cfa5032a5e8fdf7e2c5310d2866fa60a33d505
Carlos Sánchez Abad <98030188+thecharlie00@users.noreply.github.com>
2022-12-18 13:08:25 +0100

    Pantalla de login y register diseñadas
    Programar botones, recycle view y que se guarde al nombre deusuario y salga en pantalla

--- app/src/main/java/com/example/streamcomics/LoginRegister.kt
+++ app/src/main/java/com/example/streamcomics/LoginRegister.kt
@@ -1,11 +1,23 @@
 package com.example.streamcomics
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.streamcomics.databinding.ActivityLoginRegisterBinding
+import com.example.streamcomics.databinding.ActivityMainBinding
 
+private lateinit var binding: ActivityLoginRegisterBinding
 class LoginRegister : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_register)
+        binding =  ActivityLoginRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.toLogin.setOnClickListener{
+            startActivity(Intent(this, Login::class.java))
+        }
+        binding.toRegister.setOnClickListener{
+            startActivity(Intent(this, Register::class.java))
+        }
     }
 }
