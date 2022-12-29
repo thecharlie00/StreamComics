711760d6964ea8967030a6f954d6f4a8c38ff89a
Carlos Sánchez Abad <98030188+thecharlie00@users.noreply.github.com>
2022-12-29 14:10:12 +0100

    ultimos cambios del anyo
    Cambios de año nuevo

--- app/src/main/java/com/example/streamcomics/Register.kt
+++ app/src/main/java/com/example/streamcomics/Register.kt
@@ -10,19 +10,39 @@ import com.google.firebase.auth.FirebaseAuth
 
 class Register : AppCompatActivity() {
     private lateinit var binding: ActivityRegisterBinding
+    private  lateinit var firebaseAuth: FirebaseAuth
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        firebaseAuth = FirebaseAuth.getInstance()
 
         binding.backRegister.setOnClickListener {
             startActivity(Intent(this, LoginRegister::class.java))
         }
         binding.nextRegister.setOnClickListener {
-            startActivity(Intent(this, Library::class.java))
+            val email = binding.mailRegister.text.toString()
+            val password = binding.registerPassword.text.toString()
+            val confirmPassword = binding.registerPassword2.text.toString()
+
+            if(email.isNotEmpty() && password.isNotEmpty() && confirmPassword.isNotEmpty()){
+                if(password == confirmPassword){
+                    firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener() {
+                        if(it.isSuccessful){
+                            val intent = Intent(this, Login::class.java)
+                            startActivity(intent)
+                        }else{
+                            Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }else{
+                   Toast.makeText(this, "Password is not matching", Toast.LENGTH_SHORT).show()
+                }
+            }else{
+                Toast.makeText(this, "Empty fills are not allowed", Toast.LENGTH_SHORT).show()
+            }
         }
 
 
